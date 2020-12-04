1af129bee370098ab3b5182c616e111b87f55297
Jakub Wiśniewski <55205186+kubaczak@users.noreply.github.com>
2020-12-04 14:16:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,14 +17,14 @@ def init():
         print("")
 
     a = """[paths]
-    work = /tmp/pywws
-    datastoretype = filedata
+work = /tmp/pywws
+datastoretype = filedata
 
-    [config]
-    usb activity margin = 3.0
-    ws type = 1080
-    pressure offset = 21
-    logdata sync = 1"""
+[config]
+usb activity margin = 3.0
+ws type = 1080
+pressure offset = 21
+logdata sync = 1"""
 
     p = open("weather/weather.ini", "w")
     p.write(a)
